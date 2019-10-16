6153d62b3d07223029b881299a52adbc340fddc5
John Amaral <john3r8amaral@yahoo.com>
2019-10-16 19:41:46 -0400

    Fixed game over, changing all stats

--- test.js
+++ test.js
@@ -1,31 +1,30 @@
 // test.js - sandbox for testing core functionality of text-based simulation
 
-// Timer for decreasing hunger
-//var hungerTimer = setInterval('decreaseHunger()', 1000);
-
 // Code Mirror - command prompt functionality
 var commandPrompt;
-//var HUNGER = 20;
-//var FOOD = 5;
 
+// Stats and other values to keep track of
 var Values = {
     HUNGER: 20,
-    FOOD: 5
+    FOOD: 5,
+    SECURITY: 3,
+    POPULATION: 1000,
+    MILITARY: 100,
+    SCIENCE: 50
 };
 
+// Commands to choose from
 var commands = ["eat", "raiseSecurity", "execute"];
 
+// Timer to update game - called with eack tick
 var gameTickUpdate;
 
-
+// Initialize commandPrompt and game ticks
 function init(){
 
     commandPrompt = CodeMirror.fromTextArea(document.getElementById("commandPrompt"),{
-        lineNumbers : true
-    });/*CodeMirror(document.getElementById("commandPrompt"), {
-        value: "Hello, world!",
-        mode: "javascript"
-    });*/
+        lineNumbers : false
+    });
     commandPrompt.setOption("extraKeys",{
        Enter: function(cm){
            var line = commandPrompt.getLine(0);
@@ -34,38 +33,65 @@ function init(){
            commandPrompt.clearHistory();
        }
     });
+    commandPrompt.
 
     gameTickUpdate = setInterval('update()', 1000);
 }
 
+// Update the game state - called at each game tick
 function update(){
-    decreaseHunger();
-
+    if(Values.POPULATION <= 0) decreaseHunger(2);
+    else if (Values.SCIENCE <= 0 && Values.MILITARY > 0) {
+        decreaseMilitary(10);
+        decreasePopulation(75);
+        decreaseHunger(1);
+    }
+    else if (Values.MILITARY <= 0 && Values.SCIENCE > 0) {
+        decreasePopulation(75);
+        decreaseHunger(1);
+    }
+    else if (Values.MILITARY <= 0 && Values.SCIENCE <= 0) {
+        decreasePopulation(100);
+        decreaseHunger(1);
+    }
+    else {
+        decreaseHunger(1);
+        decreasePopulation(50);
+        decreaseMilitary(5);
+        decreaseScience(5);
+    }
 }
 
-/*
 // Decrease hunger
-function decreaseHunger() {
-    var hungerVal = parseInt(document.getElementById("hunger").innerHTML);
-    hungerVal -= 1;
-    document.getElementById("hunger").innerHTML = hungerVal.toString();
+function decreaseHunger(numDecrease){
 
-    // If hunger reaches 0, stop timer and game is over
-    if(hungerVal === 0) {
-        clearInterval(hungerTimer);
-        document.getElementById("gameOver").style.display = "inline";
-    }
-}*/
-
-function decreaseHunger(){
-    Values.HUNGER -= 1;
-    document.getElementById("hunger").innerHTML = Values.HUNGER;
-
-
+    // If Hunger reaches 0, trigger Game Over
     if(Values.HUNGER <= 0){
         clearInterval(gameTickUpdate);
         document.getElementById("gameOver").style.display = "inline";
+        // Somehow disable commands from being entered
+    } else {
+        Values.HUNGER -= numDecrease;
+        document.getElementById("hunger").innerHTML = Values.HUNGER;
     }
+}
+
+// Decrease population
+function decreasePopulation(numDecrease){
+    Values.POPULATION -= numDecrease;
+    document.getElementById("population").innerHTML = Values.POPULATION;
+}
+
+// Decrease military
+function decreaseMilitary(numDecrease){
+    Values.MILITARY -= numDecrease;
+    document.getElementById("military").innerHTML = Values.MILITARY;
+}
+
+// Decrease science
+function decreaseScience(numDecrease){
+    Values.SCIENCE -= numDecrease;
+    document.getElementById("science").innerHTML = Values.SCIENCE;
 }
 
 // Function for doing command
@@ -73,12 +99,15 @@ function startCommand(event) {
     var command = commandPrompt.getValue();
 
     // Eat() command
-    if(command === "Eat()") {
-        //FOOD -= 1;
+    if(command === "Eat()" && Values.FOOD > 0) {
         Values.FOOD -= 1;
         document.getElementById("foodLeft").innerHTML = Values.FOOD;
         Values.HUNGER += 5;
         document.getElementById("hunger").innerHTML = Values.HUNGER;
+    }
+    else if(command === "RaiseSecurity()" && Values.SECURITY < 5)  {
+        Values.SECURITY += 1;
+        document.getElementById("security").innerHTML = Values.SECURITY;
     }
 }
 
@@ -87,12 +116,15 @@ function matchCommand(command){
     var lbpos = command.indexOf("(");
     var arg = actual.substr(lbpos, actual.length-1);
     actual = actual.substr(0, lbpos);
+
     //Break the command into the command body and argument.
     console.log(arg);
     console.log(actual);
     for(var s of commands){
+
         //Do regex, or just direct comparison.
         if(command === s){
+
             //Process command?
         }
     }
