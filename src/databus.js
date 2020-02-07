c0187c1df2f6687199d77f7f5a4bee4c39010656
Alligrater <AtXiaoLi@outlook.com>
2020-02-07 14:57:48 -0500

    Singleton Data Manager

--- src/databus.js
+++ src/databus.js
@@ -113,6 +113,14 @@ class ValueData{
     setValue(value){
         this._value = value;
     }
+}
 
+var LIST_OF_VALS = ["Hunger","Food", "Security", "Population", "Military", "Science"];
 
+function addToValue(key, value){
+    DataManager.getInstance().addToValue(key, value);
+}
+
+function subtractFromValue(key, value){
+    DataManager.getInstance().addToValue(key, -value);
 }
