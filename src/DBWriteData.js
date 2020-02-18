c5fd3a3b60ec876de321fb303fa6008f09cb4e1e
Grace Pelella <gmpelella@wpi.edu>
2020-02-18 16:11:41 -0500

    db in progress

---
+++ src/DBWriteData.js
@@ -0,0 +1,28 @@
+class dbWriter{
+
+
+    constructor() {
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyA3Uy2QL4_4UB6tecOyWiVLs90U6c_md_w",
+            authDomain: "rficc-mqp.firebaseapp.com",
+            databaseURL: "https://rficc-mqp.firebaseio.com",
+            projectId: "rficc-mqp",
+            storageBucket: "rficc-mqp.appspot.com",
+            messagingSenderId: "545693842156",
+            appId: "1:545693842156:web:63ee85a180b3a8d45a83c9",
+            measurementId: "G-N0MDBXX2Z5"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
+    }
+
+
+
+
+
+
+
+}
+
