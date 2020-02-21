f06cc194ee142b1f782ff18451e73ff538edfd35
Grace Pelella <gmpelella@wpi.edu>
2020-02-21 16:50:27 -0500

    d a t a b a s e i s n e s t e d

--- src/game-core.js
+++ src/game-core.js
@@ -298,7 +298,7 @@ function getNextPrompt() {
 
 
     //getting info for firebase
-    if(currPrompt === prompts.MetalAge1 || prompts.ConqueringAge1  || prompts.IndustrialAge1 || prompts.SpaceAge1 || prompts.finish){
+    if((currPrompt === prompts.MetalAge1) || (currPrompt === prompts.ConqueringAge1)  || (currPrompt === prompts.IndustrialAge1) || (currPrompt === prompts.SpaceAge1) || (currPrompt === prompts.finish)){
         //if start of new age, push age choices and empty list
         //TODO: need to gather the data for at the end of the game
         ageList.push(getStatsPerAge(ageChoices));
