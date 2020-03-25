0f4831dd318338f615e75509fae9e7327d156580
Grace Pelella <gmpelella@wpi.edu>
2020-03-25 15:30:15 -0400

    newlines in manual call and potential man counter

--- src/game-core.js
+++ src/game-core.js
@@ -34,6 +34,7 @@ let tv;
 //list of choices in an age
 let ageChoices = [];
 let ageList = [];
+//todo: count manual use in custom functions maybe?
 let manCount = 0;
 let aiCount = 0;
 
@@ -74,11 +75,11 @@ function init(){
     commandPrompt.setSize('100%', '100%');
 
     let manual = {
-        "secure": "// Usage: secure(). Increase security by a level, when possible. The max security is level 5.",
-        "eat": "// Usage: eat(). Decrease food by 1, but also increase hunger by a small amount.",
-        "choose": "// Usage: choose(id), Make a choice from the list of choice given.",
-        "man": "// Usage: man(command). Check the command's usage and description.",
-        "legend": "// Usage: legend(). Look at the legend for the bar visual, with color coding for each statistic.",
+        "secure": "// Usage: secure(). Increase security by a level, when possible. The max security is level 5.\n",
+        "eat": "// Usage: eat(). Decrease food by 1, but also increase hunger by a small amount.\n",
+        "choose": "// Usage: choose(id), Make a choice from the list of choice given.\n",
+        "man": "// Usage: man(command). Check the command's usage and description.\n",
+        "legend": "// Usage: legend(). Look at the legend for the bar visual, with color coding for each statistic.\n",
         "automate": "// Usage: automate(function(){}) Automate an anonymous function to run with each game update, if the conditions are met."
     };
 
@@ -546,11 +547,9 @@ function getStatsPerAge(listOfChoiceStats){
     currentAgeStats.push(listOfChoiceStats);
     return currentAgeStats;
 }
-
 function writeResults(){
     let thingToWrite = new dbWriter();
-    //todo:// add manuel counter, add ai counter
-    thingToWrite.writePerSession(ageList);
+    thingToWrite.writePerSession(ageList,manCount,aiCount);
     //redirect to survey
     window.location.href = thingToWrite.redirect();
 
