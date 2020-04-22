e0b3f651b1997522997bbcc0a27751416b279901
Grace Pelella <gmpelella@wpi.edu>
2020-04-22 10:49:38 -0400

    empty automate(): in the manual once unlocked - fixed

--- src/customFunctions.js
+++ src/customFunctions.js
@@ -1,7 +1,7 @@
 const manual = {
     "secure": "// Usage: secure(). Increase security by a level, when possible. The max security is level 5.\n",
     "eat": "// Usage: eat(). Decrease food by 1, but also increase hunger by a small amount.\n",
-    "choose": "// Usage: choose(id), Make a choice from the list of choice given.\n",
+    "choose": "// Usage: choose(id) or option(id), Make a choice from the list of choice given.\n",
     "man":"// Usage: man(command). Check the command's usage and description.\n",
     "legend": "// Usage: legend(). Look at the legend for the bar visual, with color coding for each statistic.\n",
     "automate": "// Usage: automate(function(){}) Automate an anonymous function to run with each game update, if the conditions are met."
@@ -148,7 +148,7 @@ let FunctionManager = (function () {
                     } else {
                         appendText(commandPrompt, "\n\n// secure(): " + manual.secure + "\n// eat(): " + manual.eat +
                             "\n// choose(id): " + manual.choose + "\n// man(): " + manual.man + "\n// legend(): " + manual.legend +
-                            "// automate(): " + "\n\n>");
+                            "// automate(): " + manual.automate +"\n\n>");
                     }
                 } else {
                     let str = funName.name;
